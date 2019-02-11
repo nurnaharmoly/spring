1f3fbdb019d6e4057656e80d65ecf8f54d7577d8
nurhaharmoly <nurnaharmoly@gmail.com>
2019-02-11 13:17:47 +0600

    Studentmvc

--- studentmvcproject/src/main/java/com/example/studentmvcproject/Student.java
+++ studentmvcproject/src/main/java/com/example/studentmvcproject/Student.java
@@ -1,11 +1,12 @@
 package com.example.studentmvcproject;
 
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
@@ -19,21 +20,41 @@ public class Student {
    @Size(min = 2, max = 30,message = "Hey, Size must be between 2 and 30")
    private String name;
 
-   @NotNull(message = "Enter An Email Address")
+   @NotEmpty(message = "Enter An Email Address")
    @Email
    @Column(name = "email", unique = true)
    private String email;
 
-   private int phone;
-
    @Min(value = 18,message = "Hey, Minium Age is 18")
    private int age;
 
+   @NotNull(message = "Enter Gender")
    private String gender;
 
+   @NotEmpty(message = "Enter A Course at least")
    private String[] courseComplete;
 
-   private String Round;
+   @NotEmpty(message = "Enter Round")
+   private String round;
+
+   @Temporal(TemporalType.TIMESTAMP)
+   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date regiDate;
+
+   @LastModifiedDate
+   @Temporal(TemporalType.TIMESTAMP)
+   @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+   private Date lastModifiedDate = new Date();
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
+
+
+    private long fileSize;
+    private String fileName;
+    private String filePath;
+    private String fileExtension;
 
 
     public Long getId() {
@@ -60,13 +81,7 @@ public class Student {
         this.email = email;
     }
 
-    public int getPhone() {
-        return phone;
-    }
 
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
 
     public int getAge() {
         return age;
@@ -92,46 +107,89 @@ public class Student {
         this.courseComplete = courseComplete;
     }
 
+
     public String getRound() {
-        return Round;
+        return round;
     }
 
     public void setRound(String round) {
-        Round = round;
+        this.round = round;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return phone == student.phone &&
-                age == student.age &&
-                Objects.equals(id, student.id) &&
-                Objects.equals(name, student.name) &&
-                Objects.equals(email, student.email) &&
-                Objects.equals(gender, student.gender) &&
-                Arrays.equals(courseComplete, student.courseComplete) &&
-                Objects.equals(Round, student.Round);
+    public Date getRegiDate() {
+        return regiDate;
     }
 
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(id, name, email, phone, age, gender, Round);
-        result = 31 * result + Arrays.hashCode(courseComplete);
-        return result;
+    public void setRegiDate(Date regiDate) {
+        this.regiDate = regiDate;
     }
 
-    public Student(@NotNull @Size(min = 2, max = 30, message = "Hey, Size must be between 2 and 30") String name, @NotNull(message = "Enter An Email Address") @Email String email, int phone, @Min(value = 18, message = "Hey, Minium Age is 18") int age, String gender, String[] courseComplete, String round) {
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileExtension() {
+        return fileExtension;
+    }
+
+    public void setFileExtension(String fileExtension) {
+        this.fileExtension = fileExtension;
+    }
+
+    public Student() {
+    }
+
+    public Student(@NotNull @Size(min = 2, max = 30, message = "Hey, Size must be between 2 and 30") String name, @NotNull(message = "Enter An Email Address") @Email String email, @Min(value = 18, message = "Hey, Minium Age is 18") int age, @NotNull(message = "Enter Gender") String gender, @NotNull(message = "Enter A Course at least") String[] courseComplete, @NotNull(message = "Enter Round") String round, Date regiDate, Date lastModifiedDate, Date birthDate, long fileSize, String fileName, String filePath, String fileExtension) {
         this.name = name;
         this.email = email;
-        this.phone = phone;
         this.age = age;
         this.gender = gender;
         this.courseComplete = courseComplete;
-        Round = round;
+        this.round = round;
+        this.regiDate = regiDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.birthDate = birthDate;
+        this.fileSize = fileSize;
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.fileExtension = fileExtension;
     }
-
 
     @Override
     public String toString() {
@@ -139,14 +197,46 @@ public class Student {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", phone=" + phone +
                 ", age=" + age +
                 ", gender='" + gender + '\'' +
                 ", courseComplete=" + Arrays.toString(courseComplete) +
-                ", Round='" + Round + '\'' +
+                ", round='" + round + '\'' +
+                ", regiDate=" + regiDate +
+                ", lastModifiedDate=" + lastModifiedDate +
+                ", birthDate=" + birthDate +
+                ", fileSize=" + fileSize +
+                ", fileName='" + fileName + '\'' +
+                ", filePath='" + filePath + '\'' +
+                ", fileExtension='" + fileExtension + '\'' +
                 '}';
     }
 
-    public Student() {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                fileSize == student.fileSize &&
+                Objects.equals(id, student.id) &&
+                Objects.equals(name, student.name) &&
+                Objects.equals(email, student.email) &&
+                Objects.equals(gender, student.gender) &&
+                Arrays.equals(courseComplete, student.courseComplete) &&
+                Objects.equals(round, student.round) &&
+                Objects.equals(regiDate, student.regiDate) &&
+                Objects.equals(lastModifiedDate, student.lastModifiedDate) &&
+                Objects.equals(birthDate, student.birthDate) &&
+                Objects.equals(fileName, student.fileName) &&
+                Objects.equals(filePath, student.filePath) &&
+                Objects.equals(fileExtension, student.fileExtension);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, name, email, age, gender, round, regiDate, lastModifiedDate, birthDate, fileSize, fileName, filePath, fileExtension);
+        result = 31 * result + Arrays.hashCode(courseComplete);
+        return result;
     }
 }
--- studentmvcproject/src/main/java/com/example/studentmvcproject/StudentController.java
+++ studentmvcproject/src/main/java/com/example/studentmvcproject/StudentController.java
@@ -7,11 +7,24 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import javax.validation.Valid;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
 
 @Controller
 public class StudentController {
+
+    private static String UPLOADED_FOLDER = "D:/My Git/spring/studentmvcproject/src/main/resources/static/images/ ";
+
+    @Autowired
+    private ImageOptimizer imageOptimizer;
+
     @Autowired
     private StudentRepo repo;
 
@@ -21,13 +34,35 @@ public class StudentController {
     }
 
     @PostMapping("/add")
-    public String save(@Valid Student student, BindingResult bindingResult, Model model){
+    public String save(@Valid Student student, BindingResult bindingResult, Model model, @RequestParam("file") MultipartFile file){
         if(bindingResult.hasErrors()){
             return "add-page";
+        }else {
+
+            student.setRegiDate(new Date());
+
+
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+
+                Files.write(path, bytes);
+                student.setFileName("new-" + file.getOriginalFilename());
+                student.setFileSize(file.getSize());
+
+                student.setFilePath("images/" + "new-" + file.getOriginalFilename());
+                student.setFileExtension(file.getContentType());
+
+
+                this.repo.save(student);
+                model.addAttribute("student", new Student());
+                model.addAttribute("succMsg", "Congratulation! You are old enough to sign up for this site.");
+                imageOptimizer.optimizeImage(UPLOADED_FOLDER, file, 0.5f, 100, 100);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        this.repo.save(student);
-        model.addAttribute("student", new Student());
-        model.addAttribute("SuccMsg", "Congratulation! You are old enough to sign up for this site.");
         return "add-page";
     }
 
@@ -50,6 +85,9 @@ public class StudentController {
         @GetMapping(value = "/")
     public String index(Model model){
         model.addAttribute("list", this.repo.findAll());
+        this.repo.findAll().forEach((c) -> {
+            System.out.println(c.toString());
+        });
         return "index";
         }
 
