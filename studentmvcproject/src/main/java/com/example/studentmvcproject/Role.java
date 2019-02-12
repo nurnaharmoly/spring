67f74b3d328a0a84daa959e32204ae9296c8082d
nurhaharmoly <nurnaharmoly@gmail.com>
2019-02-12 13:37:32 +0600

    Stud

---
+++ studentmvcproject/src/main/java/com/example/studentmvcproject/Role.java
@@ -0,0 +1,55 @@
+package com.example.studentmvcproject;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Enter Role.........")
+    @Column(unique = true, name = "role_name")
+    private String roleName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) &&
+                Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, roleName);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
