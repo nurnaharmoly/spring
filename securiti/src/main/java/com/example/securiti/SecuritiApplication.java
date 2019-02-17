b3fb3c20fbb065bda7bf71bd79e069de4539720b
nurnaharmoly <nurnaharmoly@gmail>
2019-02-18 02:06:02 +0600

    secure

---
+++ securiti/src/main/java/com/example/securiti/SecuritiApplication.java
@@ -0,0 +1,13 @@
+package com.example.securiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritiApplication.class, args);
+    }
+
+}
