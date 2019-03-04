72eae76069961e55b219cb1804bce9ff2eccd82d
nurnaharmoly <nurnaharmoly@gmail>
2019-03-04 23:36:35 +0600

    themplfine

---
+++ lastthemplet/src/main/java/com/example/lastthemplet/LastthempletApplication.java
@@ -0,0 +1,13 @@
+package com.example.lastthemplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastthempletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastthempletApplication.class, args);
+    }
+
+}
---
+++ lastthemplet/src/test/java/com/example/lastthemplet/LastthempletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.lastthemplet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LastthempletApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
